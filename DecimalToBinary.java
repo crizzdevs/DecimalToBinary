b7d35b6824d8d7a604ebca6b39911c06725c13df
crizzdevs <abuancrisjulian1206@gmail.com>
2025-09-12 12:15:46 +0800

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
-public class (FILENAME) {
+public class Deccalc {
 
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            while (true) {
-                System.out.print("Enter a decimal number (or type STOP to quit): ");
-                String input = sc.nextLine();
-                
-                if (input.equalsIgnoreCase("STOP")) {
-                    System.out.println("Program terminated.");
-                    break;
-                }
-                
-                try {
-                    int decimal = Integer.parseInt(input);
-                    String binary = Integer.toBinaryString(decimal);
-                    System.out.println(decimal + " -> " + binary);
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid number. Please enter a valid integer or STOP.");
-                }
+        Scanner sc = new Scanner(System.in);
+            
+        
+        while (true) {
+            System.out.println("Decimal to Binary Calculator");
+            System.out.print("Enter a decimal number (or type STOP to quit): ");
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("STOP")) {
+                System.out.println("Program terminated.");
+                break;
+            }
+            
+            
+            try {
+                int decimal = Integer.parseInt(input);
+                String binary = Integer.toBinaryString(decimal);
+                System.out.println(decimal + " -> " + binary);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number. Please enter a valid integer or STOP.");
             }
         }
+
+        sc.close();
     }
 }
